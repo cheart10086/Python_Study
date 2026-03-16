68ea2eec92a73bb2b8ab6021a7ca6582b7c67ae0
HappyMaker <2030440313@qq.com>
2026-03-16 14:22:41 +0800

    first_commit

---
+++ Unit1/4_input.py
@@ -0,0 +1,18 @@
+#input(提示信息)输入数据，返回值为输入数据，且输入所有的数据均为字符串
+name = input("请输入姓名\n")
+age = input("请输入年龄\n")
+
+a = 5
+b = 2
+
+print(f"a+b为{a+b}\n")
+print(f"a-b为{a-b}\n")
+print(f"a*b为{a*b}\n")
+print(f"a/b为{a/b}\n")  # /为除法符号，结果为小数
+print(f"a//b为{a//b}\n") # //为整除符号
+print(f"a%b为{a%b}\n") #%为取模运算
+print(f"a**b为{a**b}\n") #** 为幂指数运算
+#运算符优先级  ** > * / // %  > + -
+#Python赋值运算符，例如+=，-=和比较运算符，例如<,>,<=与C语言一致
+
+#逻辑运算符：and，or，not
---
+++ unit2/practice1.py
@@ -0,0 +1,9 @@
+year = int(input("请输入年份"))
+
+if year%100!=0:
+    if year%4==0:
+        print(f"{year}是闰年")
+    else:
+        print(f"{year}不是闰年")
+else :
+    print(f"{year}是闰年")
